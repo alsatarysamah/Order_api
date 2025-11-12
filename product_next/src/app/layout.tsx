1da1829b6b45a382900e10d0d29759063b39a868
alsatarysamah <alsatary.samah@yahoo.com>
2025-11-12 11:15:14 +0300

    update main layout

--- product_next/src/app/layout.tsx
+++ product_next/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import Footer from "@components/Footer/Footer";
+import Header from "@components/Header/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex flex-col min-h-screen overflow-auto scroll-smooth">
+          <Header />
+          <main className="flex flex-col flex-grow text-center mx-5">
+            {children}
+          </main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
